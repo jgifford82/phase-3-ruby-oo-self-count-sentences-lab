1ce4721255fb62ec19a7abcb344cd2f8c65fa9cf
Jennifer Gifford <jgifford82@gmail.com>
2022-10-11 21:43:42 -0600

    commit tree 2684ba76d0354d2c30b0b6ca82aec65f48b057b8

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,27 @@
 require 'pry'
+# learn test --f-f
 
 class String
 
   def sentence?
-    
+    # str\z == "."
+    # str.end_with? == "."
+    # :String.end_with? != "."
+    # :String\z == "."
+    self.end_with?(".")
   end
+# binding.pry
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[!?.]\s/).count
+    # binding.pry
   end
 end
